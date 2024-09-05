02feee2af17232ea5026109a872196a2a5de12a0
Breno Alves <16504050+MetlHedd@users.noreply.github.com>
2024-09-05 19:01:09 +0000

    Refactor GET function in .well-known/host-meta/route.ts

--- app/[domain]/[handle]/.well-known/host-meta/route.ts
+++ app/[domain]/[handle]/.well-known/host-meta/route.ts
@@ -1,17 +1,14 @@
 import { redirect } from 'next/navigation';
 
-export const GET = async (
-  request: Request,
-  searchParams: { [key: string]: string | string[] | undefined }
-) => {
-    return new Response(JSON.stringify(searchParams), { status: 200 });
+export const GET = async (props: any) => {
+    return new Response(JSON.stringify(props), { status: 200 });
 
-    if (typeof searchParams == "string" ) {
+    /*if (typeof searchParams == "string" ) {
         redirect("https://fed.brid.gy/.well-known/host-meta/?"+searchParams);
     } else if(typeof searchParams == "object") {
         const searchParamsString = searchParams.toString();
         redirect("https://fed.brid.gy/.well-known/host-meta/?"+searchParamsString);
     } else {
         return new Response("Invalid searchParams type", { status: 400 });
-    }
+    }*/
 }
