e17ba1a1554ec58a736dc9a8413f009492b988b8
Breno Alves <16504050+MetlHedd@users.noreply.github.com>
2024-09-05 19:08:52 +0000

    fiiiiiiiiiiiix

--- app/[domain]/[handle]/.well-known/host-meta/route.ts
+++ app/[domain]/[handle]/.well-known/host-meta/route.ts
@@ -2,14 +2,5 @@ import { redirect } from 'next/navigation';
 import { NextRequest } from 'next/server';
 
 export const GET = async (request: NextRequest) => {
-    return new Response(JSON.stringify(request.nextUrl.searchParams.toString()), { status: 200 });
-
-    /*if (typeof searchParams == "string" ) {
-        redirect("https://fed.brid.gy/.well-known/host-meta/?"+searchParams);
-    } else if(typeof searchParams == "object") {
-        const searchParamsString = searchParams.toString();
-        redirect("https://fed.brid.gy/.well-known/host-meta/?"+searchParamsString);
-    } else {
-        return new Response("Invalid searchParams type", { status: 400 });
-    }*/
+    redirect("https://fed.brid.gy/.well-known/host-meta/?"+request.nextUrl.searchParams.toString());
 }
