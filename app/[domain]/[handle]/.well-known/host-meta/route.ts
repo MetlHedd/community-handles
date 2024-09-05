16fc3b2779cf08b46bfd09b4742890055fd46b16
Breno Alves <16504050+MetlHedd@users.noreply.github.com>
2024-09-05 18:57:04 +0000

    Add route for handling .well-known/host-meta requests

--- app/[domain]/[handle]/.well-known/host-meta/route.ts
+++ app/[domain]/[handle]/.well-known/host-meta/route.ts
@@ -4,6 +4,8 @@ export const GET = async (
   request: Request,
   searchParams: { [key: string]: string | string[] | undefined }
 ) => {
+    return new Response(JSON.stringify(searchParams), { status: 200 });
+
     if (typeof searchParams == "string" ) {
         redirect("https://fed.brid.gy/.well-known/host-meta/?"+searchParams);
     } else if(typeof searchParams == "object") {
