683517bdfc60ee17f43c9efd09c344ffea831baf
Breno Alves <16504050+MetlHedd@users.noreply.github.com>
2024-09-05 19:10:28 +0000

    typo

--- app/[domain]/[handle]/.well-known/host-meta/route.ts
+++ app/[domain]/[handle]/.well-known/host-meta/route.ts
@@ -2,5 +2,5 @@ import { redirect } from 'next/navigation';
 import { NextRequest } from 'next/server';
 
 export const GET = async (request: NextRequest) => {
-    redirect("https://fed.brid.gy/.well-known/host-meta/?"+request.nextUrl.searchParams.toString());
+    redirect("https://fed.brid.gy/.well-known/host-meta?"+request.nextUrl.searchParams.toString());
 }
